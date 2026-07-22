295462d38b1acd6dc461c5add0637243fd0c402e
zirilanzillah <zirilan@gmx.net>
2026-07-22 04:06:36 +0200

    KW 1 und alternativ KW 01 als Kommentar eingefügt
    falls der in KW52 / KW53 merkt, dass dsbmobile die URL /01/ statt /1/ verwenden sollte, bei der URL Erstellung einfach die auskommentierte Zeile Code verwenden
    Sieht man frühestens Ende 2026, wenn der Stundenplan für KW 1 KW01 erstellt wurde

--- StundenplanHintergrundbildDunkel.ps1
+++ StundenplanHintergrundbildDunkel.ps1
@@ -1,7 +1,13 @@
 # --- Konfiguration ---
 # aktuelleWoche + 7 Tage, ich will ja den Stundenplan von nächster Woche laden nicht dieser WOche ich Idiot ...
 $aktuelleWoche = (Get-Culture).Calendar.GetWeekOfYear((Get-Date).AddDays(7), [System.Globalization.CalendarWeekRule]::FirstFourDayWeek, [System.DayOfWeek]::Monday)
+
+# falls die Kalenderwoche 1 bis 9 einstellig sein sollten von der Webseite in der URL noch ; "{0:D2}" -f $aktuelleWoche zum Formatieren einfügen, siehe nächste Zeile
+# $aktuelleWoche = (Get-Culture).Calendar.GetWeekOfYear((Get-Date).AddDays(7), [System.Globalization.CalendarWeekRule]::FirstFourDayWeek, [System.DayOfWeek]::Monday); "{0:D2}" -f $aktuelleWoche
+
+# für FIS/FIA 2369 liegt der Stundenplan in diesen verschachtelten Ordnern 1e7d336d-41c7-4a32-9b08-dde7ad6df345 und f440c721-c59b-4a39-8b0a-958ee4215a59 kein login und pw für dsmobile nötig c00006 ist der 6te Eintrag von oben als 2369
 $targetUrl = "https://dsbmobile.de/data/1e7d336d-41c7-4a32-9b08-dde7ad6df345/f440c721-c59b-4a39-8b0a-958ee4215a59/$aktuelleWoche/c/c00006.htm"
+
 $tempPath = "$HOME\Documents\Stundenplan_Raw_KW$aktuelleWoche.png"
 $savePath = "$HOME\Documents\Stundenplan_KW$aktuelleWoche.png"
 $edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
@@ -13,7 +19,7 @@ Write-Host "Lade Stundenplan für KW $aktuelleWoche..." -ForegroundColor Cyan
 Get-Process msedge -ErrorAction SilentlyContinue | Stop-Process -Force
 Start-Sleep -Seconds 1
 
-# Screenshot aufnehmen (kleine Auflösung, sonst hängt der Stundenplan oben am Screen und unten ist zu viel Luft
+# Screenshot aufnehmen (kleine Auflösung, sonst hängt der Stundenplan oben am Screen und unten ist zu viel Luft, Internet ist grottig, 6 s Wartezeit für die Seite zum Laden einplanen
 $processArgs = "--headless", "--disable-gpu", "--window-size=960,540", "--screenshot=`"$tempPath`"", "`"$targetUrl`""
 Start-Process -FilePath $edgePath -ArgumentList $processArgs -NoNewWindow
 Start-Sleep -Seconds 6
@@ -59,6 +65,8 @@ for ($x = 0; $x -lt $bmp.Width; $x++) {
 
 # vergrößertes Hintergrundbild abspeichern
 $bmp.Save($savePath, [System.Drawing.Imaging.ImageFormat]::Png)
+
+# er löscht mir die Raw Datei nicht richtig weg, Überschreiben geht, k/A warum löschen nicht geht, muss ich später drüber gucken was hier hängt, Fehlermeldung einfach erstmal unterdrücken und weiter
 $bmp.Dispose()
 Remove-Item $tempPath -ErrorAction SilentlyContinue
 
